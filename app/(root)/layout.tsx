055f8270e2af317acdf40bdc94cb91b2ecd6165b
Davi Curty <davicurty850@gmail.com>
2026-07-13 17:33:29 -0300

    feat: add NavbarMobile component and integrate into RootLayout; refactor NavbarDesktop for improved layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
 /** biome-ignore-all assist/source/organizeImports: <> */
 import { NavbarDesktop } from "@/components/NavbarDesktop";
+import NavbarMobile from "@/components/NavbarMobile";
 import type { ReactNode } from "react";
 
 export default function RootLayout({children}: {children: ReactNode}) {
     return (
         <main className="">
             <NavbarDesktop />
+            <NavbarMobile />
             {children}
         </main>
     )
---
+++ components/NavbarMobile.tsx
@@ -0,0 +1,46 @@
+/** biome-ignore-all assist/source/organizeImports: <> */
+"use client";
+
+import { navLinksMobile } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavbarMobile = () => {
+  const pathname = usePathname()
+
+  return (
+    <div className="max-sm:flex hidden px-3 fixed z-50 bottom-3 left-0 w-full bg-cover justify-center items-center gap-2">
+      <div className="w-full py-2 bg-chart-2/5 backdrop-blur-xl border border-foreground/15 rounded-full mx-2"> 
+        <ul className="flex place-items-center gap-2">
+          {navLinksMobile.map((el) => {
+            const Icon = el.icon;
+    
+            const isActive =
+              el.href === "/"
+                ? pathname === "/"
+                : pathname.startsWith(el.href);
+    
+            const shouldNotTranslate = ["home"].includes(el.id);
+            const CreateRoom = ["create"].includes(el.id)
+    
+            return (
+              <li key={el.id} className="w-1/4">
+                <Link
+                  href={el.href}
+                  translate={shouldNotTranslate ? "no" : "yes"}
+                  className={`flex items-center justify-center transition`}
+                >
+                  <div className={`${CreateRoom ? "p-1.5 bg-sidebar-primary rounded-full" : ""}`}>
+                    <Icon size={30} strokeWidth={1.8} className={` ${CreateRoom ? "text-slate-900" : "text-inherit" } ${isActive ? "text-sidebar-primary" : "text-foreground"}`} />
+                  </div>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarMobile;
